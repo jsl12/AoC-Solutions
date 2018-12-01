21ca9d71c327c1970fbd5c80b0b0d2e2bc683d19
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-01 15:20:16 -0600

    initial commit for 2018

---
+++ 2018/day1.py
@@ -0,0 +1,7 @@
+def part1(input):
+    print(input)
+    return
+
+if __name__ == '__main__':
+    with open('day1_input.txt', 'r') as file:
+        part1(file.read())
--- register.py
+++ register.py
@@ -1,9 +1,8 @@
 y2017 = __import__('2017')
+y2018 = __import__('2018')
 
 REGISTRATION = {
-    'day1.1': y2017.day1.part1,
-    'day1.2': y2017.day1.part2
-    # 'day2': y2017.day2.part1,
-    # 'day3': y2017.day3.part1,
-    # 'day4': y2017.day4.part1,
+    '2017.1.1': y2017.day1.part1,
+    '2017.1.2': y2017.day1.part2,
+    '2018.1.1': y2018.day1.part1
 }
