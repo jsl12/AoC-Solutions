27d5b151278c2d8c14a972bad386437aad0dbf21
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-02 13:10:16 -0600

    semi working 2018 day 1 part 2 solution

--- 2018/day1.py
+++ 2018/day1.py
@@ -4,7 +4,35 @@ def part1(input):
         res += int(line)
     return res
 
+def part2(input):
+    input = [int(line) for line in input.splitlines()]
+    # input = [int(line) for line in input.splitlines()][:10]
+    new_f = 0
+    prev = [0]
+
+    # for c in input:
+    # while new_f not in prev:
+    while True:
+        new_f = prev[-1] + input[0]
+        if new_f in prev:
+            return new_f
+        else:
+            prev.append(new_f)
+            input = rotate(input)
+            if len(prev) > 10000:
+                return 'Failed'
+
+def rotate(input, n=1):
+    return input[n:] + input[:n]
+
 if __name__ == '__main__':
-    with open('day1_input.txt', 'r') as file:
-        res = part1(file.read())
-    print(res)
+    from pathlib import Path
+    p = Path(r'C:\Users\lanca_000\Documents\Software\Python\AoC Benchmark\AoC-Inputs\2018')
+    with open(p / 'day1.txt', 'r') as file:
+        input = file.read()
+    print(part1(input))
+    # print(part2(input))
+    print(part2('+1\n-1'))
+    print(part2('+3\n+3\n+4\n-2\n-4\n'))
+    print(part2('-6\n+3\n+8\n+5\n-6'))
+    print(part2('+7\n+7\n-2\n-7\n-4'))
