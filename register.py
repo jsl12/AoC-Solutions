e3397252e41160f6ec180ed5e5523c04e03e7069
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-02 12:19:51 -0600

    registered 2018 day 2 part 2

--- register.py
+++ register.py
@@ -5,5 +5,6 @@ REGISTRATION = {
     '2017.1.1': y2017.day1.part1,
     '2017.1.2': y2017.day1.part2,
     '2018.1.1': y2018.day1.part1,
-    '2018.2.1': y2018.day2.part1
+    '2018.2.1': y2018.day2.part1,
+    '2018.2.2': y2018.day2.part2
 }
