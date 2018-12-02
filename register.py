aa69fd0220dd55a433065f51f19c990b05aa77f5
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-02 00:54:26 -0600

    added 2018 day 2

--- register.py
+++ register.py
@@ -4,5 +4,6 @@ y2018 = __import__('2018')
 REGISTRATION = {
     '2017.1.1': y2017.day1.part1,
     '2017.1.2': y2017.day1.part2,
-    '2018.1.1': y2018.day1.part1
+    '2018.1.1': y2018.day1.part1,
+    '2018.2.1': y2018.day2.part1
 }
