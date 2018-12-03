b24344e098cfe76472a7d2b38ba2ca9751443238
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-03 14:25:16 -0600

    disabled test cases, now running with real input

--- 2018/day3.py
+++ 2018/day3.py
@@ -132,21 +132,26 @@ def part1(input):
         overlap = a.overlap(b)
         if overlap is not None:
             print(''.center(50, '='))
-            # # print('Overlapping corner between:\n{}\n{}'.format(a, b))
             print('Corner of {} is inside {}'.format(a.num, b.num))
-            hits += 1
             print(overlap)
+            hits += 1
     return hits
 
 def part2(input):
     return
 
 if __name__ == '__main__':
-    # from pathlib import Path
-    # p = Path(r'C:\Users\lanca_000\Documents\Software\Python\AoC Benchmark\AoC-Inputs\2018')
-    # with open(p / 'day3.txt', 'r') as file:
-    #     input = file.read()
-    # print(part1(input))
+    from pathlib import Path
+    p = Path(r'C:\Users\lanca_000\Documents\Software\Python\AoC Benchmark\AoC-Inputs\2018')
+    with open(p / 'day3.txt', 'r') as file:
+        input = file.read()
+    print(part1(input))
     # print(part2(input))
     # print(part1('#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2'))
-    print(part1('#0 @ 0,0: 4x3\n#1 @ 4,0: 4x3\n#2 @ 2,2: 4x4\n#3 @ 0,5: 3x2\n#4 @ 5,5: 4x2\n'))
+    # print(part1('#0 @ 0,0: 4x3\n#1 @ 4,0: 4x3\n#2 @ 2,2: 4x4\n#3 @ 0,5: 3x2\n#4 @ 5,5: 4x2\n'))
+    # test_boxes = [
+    #     '#1 @ 2,2: 4x4',
+    #     '#2 @ 5,5: 3x2'
+    # ]
+    # ol = Claim(test_boxes[0]).overlap(Claim(test_boxes[1]))
+    # print(ol)
