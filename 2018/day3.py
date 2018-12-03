71933a8a078f0d94e71d3acfd2c66cb2682674e3
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-03 13:11:42 -0600

    scanning hits now works with example given

--- 2018/day3.py
+++ 2018/day3.py
@@ -15,7 +15,7 @@ class Claim:
         self.iter_pos[0] -= 1
 
     def x_end(self):
-        return self.coords[0] + self.size[0]
+        return self.coords[0] + self.size[0] - 1
 
     def x_check(self, x=None):
         if x is None:
@@ -23,7 +23,7 @@ class Claim:
         return self.coords[0] <= x <= self.x_end()
 
     def y_end(self):
-        return self.coords[1] + self.size[1]
+        return self.coords[1] + self.size[1] - 1
 
     def y_check(self, y=None):
         if y is None:
@@ -56,6 +56,8 @@ class Claim:
                 self.iter_pos[0] = self.coords[0]
                 # Increment row
                 self.iter_pos[1] += 1
+
+            # Check to make sure still in bounds, only necessary to check y
             if not self.y_check():
                 return False
             else:
@@ -66,22 +68,31 @@ class Claim:
 
 def part1(input):
     scanned = []
+    hits = []
     for line in input.splitlines():
         c = Claim(line)
         print(c.num)
+
+        # For each coordinate in the claim
         for x, y in c:
-            print(x, y)
-        # print(c.coords)
-        # print(c.size)
-    return
+            # print(x, y)
+            # For each claim already scanned
+            for prev in scanned:
+                # If the coordinate is a hit
+                if prev.check(x, y):
+                    # Append the coordinates to the list of hits
+                    hits.append((x, y))
+        scanned.append(c)
+    return len(hits)
 
 def part2(input):
     return
 
 if __name__ == '__main__':
-    from pathlib import Path
-    p = Path(r'C:\Users\lanca_000\Documents\Software\Python\AoC Benchmark\AoC-Inputs\2018')
-    with open(p / 'day3.txt', 'r') as file:
-        input = file.read()
-    print(part1(input))
-    print(part2(input))
+    # from pathlib import Path
+    # p = Path(r'C:\Users\lanca_000\Documents\Software\Python\AoC Benchmark\AoC-Inputs\2018')
+    # with open(p / 'day3.txt', 'r') as file:
+    #     input = file.read()
+    # print(part1(input))
+    # print(part2(input))
+    print(part1('#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2'))
