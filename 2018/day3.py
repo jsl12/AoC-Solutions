848d023946f5ff98798133c7358b0f76e6ede9b9
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-03 12:50:04 -0600

    First pass at day 3, making each Claim iterable

--- 2018/day3.py
+++ 2018/day3.py
@@ -1,4 +1,68 @@
+import re
+
+class Claim:
+    REGEX = re.compile('#(\d+) @ (\d+),(\d+): (\d+)x(\d+)')
+
+    def __init__(self, claim):
+        self.text = claim
+        match = self.REGEX.match(self.text)
+        self.num = int(match.group(1))
+        self.coords = (int(match.group(2)), int(match.group(3)))
+        self.size = (int(match.group(4)), int(match.group(5)))
+
+        self.iter_pos = list(self.coords)
+        # Moves the pointer back a position so that next() returns the first value
+        self.iter_pos[0] -= 1
+
+    def x_end(self):
+        return self.coords[0] + self.size[0]
+
+    def x_check(self, x=None):
+        if x is None:
+            x = self.iter_pos[0]
+        return self.coords[0] <= x <= self.x_end()
+
+    def y_end(self):
+        return self.coords[1] + self.size[1]
+
+    def y_check(self, y=None):
+        if y is None:
+            y = self.iter_pos[1]
+        return self.coords[1] <= y <= self.y_end()
+
+    def check(self, x, y):
+        hor = self.x_check(x)
+        ver = self.y_check(y)
+        return hor and ver
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.increment_pointer():
+            return self.iter_pos
+        else:
+            raise StopIteration
+
+    def increment_pointer(self):
+        # Move to the right once
+        self.iter_pos[0] += 1
+        # Determine if at end of row
+        if not self.check(self.iter_pos[0], self.iter_pos[1]):
+            return False
+        else:
+            return True
+
+
 def part1(input):
+    scanned = []
+    for line in input.splitlines():
+        c = Claim(line)
+        print(c.num)
+        for x, y in c:
+            print(x, y)
+        # print(c.coords)
+        # print(c.size)
     return
 
 def part2(input):
