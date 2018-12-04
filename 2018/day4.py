ee80d3114e78fabe3227cc6d96c56d4df013a514
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-04 00:50:36 -0600

    now producing the event DataFrame from the input with event_df

--- 2018/day4.py
+++ 2018/day4.py
@@ -1,5 +1,31 @@
+import re
+import pandas as pd
+from datetime import datetime
+
+DATE_REGEX = re.compile('\[(\d+-\d+-\d+ \d+:\d+)\]')
+BEGIN_REGEX = re.compile('.+Guard #(\d+)')
+EVENT_REGEX = re.compile('.*(begins shift|falls asleep|wakes up)')
+
 def part1(input):
+    df = event_df(input)
     return
+
+def event_df(input):
+    df = pd.DataFrame([proc_line(line) for line in input.splitlines()])
+    df = df.set_index('Date').sort_index()
+    return df
+
+def proc_line(line):
+    try:
+        g = int(BEGIN_REGEX.match(line).group(1))
+    except AttributeError:
+        g = None
+    res = {
+        'Date': datetime.strptime(DATE_REGEX.match(line).group(1), '%Y-%m-%d %H:%M'),
+        'Guard': g,
+        'Event': EVENT_REGEX.match(line).group(1)
+    }
+    return res
 
 def part2(input):
     return
@@ -10,4 +36,4 @@ if __name__ == '__main__':
     with open(p / 'day4.txt', 'r') as file:
         input = file.read()
     print(part1(input))
-    print(part2(input))
+    # print(part2(input))
