3e3bf78dbd9763f1a43056383ffcfcc9824bed11
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-04 00:19:31 -0600

    starting 2018 day 4

---
+++ 2018/day4.py
@@ -0,0 +1,13 @@
+def part1(input):
+    return
+
+def part2(input):
+    return
+
+if __name__ == '__main__':
+    from pathlib import Path
+    p = Path(r'C:\Users\lanca_000\Documents\Software\Python\AoC Benchmark\AoC-Inputs\2018')
+    with open(p / 'day4.txt', 'r') as file:
+        input = file.read()
+    print(part1(input))
+    print(part2(input))
