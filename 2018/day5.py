968f7769a3076df275acb0a3bd83133daa49b21f
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-05 12:41:49 -0600

    pulled out react

--- 2018/day5.py
+++ 2018/day5.py
@@ -1,4 +1,7 @@
 def part1(input):
+    return len(react(input))
+
+def react(input):
     res = input.replace('\n', '')
     i = 0
 
@@ -8,14 +11,14 @@ def part1(input):
         if 97 <= ord(res[i]) <= 122:
             # lower case letter
             if res[i + 1] == res[i].upper():
-                res = res[:i] + res[i+2:]
+                res = res[:i] + res[i + 2:]
                 i -= 1
             else:
                 i += 1
         elif 65 <= ord(res[i]) <= 90:
             # upper case letter
             if res[i + 1] == res[i].lower():
-                res = res[:i] + res[i+2:]
+                res = res[:i] + res[i + 2:]
                 i -= 1
             else:
                 i += 1
@@ -27,10 +30,7 @@ def part1(input):
 
     while i < len(res) - 1:
         check()
-
-    with open('res.txt', 'w') as file:
-        file.write(res)
-    return len(res)
+    return res
 
 def part2(input):
     return
