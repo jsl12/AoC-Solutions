cbbe2adc999bfab783d29cfdfbfef924273883b3
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-10 19:32:34 -0600

    initial commit for day 10

---
+++ 2018/day10.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def part1(input):
+    return
+
+def part2(input):
+    return
+
+if __name__ == '__main__':
+    import input as inp
+    DAY = 10
+    input = inp.read(DAY)
+    print(part1(input))
+    # print(part2(input))
