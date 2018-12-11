5adc3ed036e8cc035506e3b787658561e6feeea7
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-10 22:10:48 -0600

    visualizer roughed out. need to switch to pandas

--- 2018/day10.py
+++ 2018/day10.py
@@ -1,18 +1,32 @@
 import re
-import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 REGEX = re.compile('position=< ?([-\d]+), +([-\d]+)> velocity=< ?([-\d]+), +([-\d]+)>')
 
-def parse(line):
-    match = REGEX.match(line)
-    traj = {
-        'pos': (match.group(2), match.group(3)),
-        'vel': (match.group(3), match.group(4))
-    }
-    return traj
+class Point:
+    def __init__(self, line):
+        match = REGEX.match(line)
+        self.pos = (int(match.group(2)), int(match.group(3)))
+        self.vel = (int(match.group(3)),int(match.group(4)))
+
+    def advance(self, t=1):
+        self.pos = (
+            self.pos[0] + (self.vel[0] * t),
+            self.pos[1] + (self.vel[1] * t)
+        )
+
+def visualize(points, step, n):
+    df = pd.DataFrame({'x': [pt.pos[0] for pt in points], 'y': [pt.pos[1] for pt in points]}).set_index('x')
+    fig, ax = plt.subplots(figsize=(19.2, 10.8))
+    ax.plot(df, '.')
+    fig.savefig('day10.png')
+    plt.close(fig)
 
 def part1(input):
-    input = [parse(line) for line in input.splitlines()]
+    pts = [Point(line) for line in input.splitlines()]
+    [pt.advance(1000) for pt in pts]
+    visualize(pts)
 
     return
 
