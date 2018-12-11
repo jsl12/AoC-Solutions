021acea673fe31e85761a11ca670b3e3f6eec0bb
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-11 10:05:25 -0600

    greatly sped things up with pandas

--- 2018/day10.py
+++ 2018/day10.py
@@ -3,18 +3,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-def visualize(pos_df, n, fig=None, ax=None, close=True):
-    df = pos_df[n]
+def visualize(pos_df, n, type='single', fig=None, ax=None, close=True):
     if fig is None and ax is None:
         fig, ax = plt.subplots(figsize=(19.2, 10.8))
-    ax.plot(df, '.')
+    if type == 'single':
+        ax.plot(pos_df['x'][n], pos_df['y'][n], '.')
+    elif type == 'moving':
+        for i, row in pos_df.iterrows():
+            ax.plot(row['x'][:n], row['y'][:n], 'b')
     fig.savefig('day10.png')
     if close:
         plt.close(fig)
     else:
         return fig, ax
 
-def make_dfs(input, n=1000):
+def make_dfs(input, n):
     REGEX = re.compile('position=< ?([-\d]+), +([-\d]+)> velocity=< ?([-\d]+), +([-\d]+)>')
     lines = input.splitlines()
     length = len(lines)
@@ -36,22 +39,34 @@ def make_dfs(input, n=1000):
         y[i] = point['pos']['y']
         x_vel[i] = point['vel']['x']
         y_vel[i] = point['vel']['y']
-    vel_df = pd.DataFrame({'x': x_vel, 'y': y_vel})
-
+    df = pd.DataFrame({
+        'x': x,
+        'y': y,
+        'x_vel': x_vel,
+        'y_vel': y_vel
+    })
     # construct position over time df
-    idx = pd.MultiIndex.from_product([['x', 'y'], np.arange(n)])
+    if isinstance(n, tuple):
+        r = np.arange(n[0], n[1], dtype=np.int32)
+    elif isinstance(n, int):
+        r = np.arange(n, dtype=np.int32)
+    idx = pd.MultiIndex.from_product([['x', 'y'], np.arange(r[0], r[0] + r.size)])
     pos_df = pd.DataFrame(columns=idx, index=np.arange(length))
-    for (axis, t), values in pos_df.iteritems():
-        if axis == 'x':
-            pos_df.loc[:, (axis, t)] = x + (t * x_vel)
-        elif axis == 'y':
-            pos_df.loc[:, (axis, t)] = y + (t * y_vel)
+    df = pd.DataFrame(data=np.empty((length, r.size), dtype=np.int32))
+    df.columns = df.columns + r[0]
+    for i, row in df.iterrows():
+        df.loc[i] = r
+
+    pos_df['x'] = df.multiply(x_vel, axis=0).add(x, axis=0)
+    pos_df['y'] = df.multiply(y_vel, axis=0).add(y, axis=0)
 
     return pos_df
 
 def part1(input):
-    dfp = make_dfs(input, n=5000)
-    visualize(dfp, 1000)
+    dfp = make_dfs(input, n=(8000, 10000))
+    fig, ax = visualize(dfp, 9000, close=False)
+    for i in range(10):
+        fig, ax = visualize(dfp, 200*i, close=False, fig=fig, ax=ax)
     return
 
 def part2(input):
