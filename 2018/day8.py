e0c3fb2a336fac60567a5b07244a4b6b265afd4f
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-12 18:33:41 -0600

    factored out use of nonlocal

--- 2018/day8.py
+++ 2018/day8.py
@@ -3,9 +3,8 @@ import numpy as np
 def part1(input):
     input = np.array([int(i) for i in input.split(' ')], dtype=np.uint8)
 
-    def sum_node(iterator):
+    def sum_node(input, iterator):
         # assumes start of node
-        nonlocal input
         count_child = input[iterator.iterindex]
         count_md = input[iterator.iterindex+1]
         total = 0
@@ -13,13 +12,12 @@ def part1(input):
         iterator.iterindex += 2
         if count_child > 0:
             for i in range(count_child):
-                total += sum_node(iterator)
-        total += sum_metadata(iterator, count_md)
+                total += sum_node(input, iterator)
+        total += sum_metadata(input, iterator, count_md)
         return total
 
-    def sum_metadata(iterator, count):
+    def sum_metadata(input, iterator, count):
         # assumes start of metadata
-        nonlocal input
         start = iterator.iterindex
         end = start + count
         if iterator.itersize > end:
@@ -31,7 +29,7 @@ def part1(input):
 
     with np.nditer(input, op_flags=['readonly'], flags=['f_index']) as it:
         while not it.finished:
-            total = sum_node(it)
+            total = sum_node(input, it)
     return total
 
 def part2(input):
