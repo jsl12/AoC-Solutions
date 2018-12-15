c4f633872e6ad181da95f300adfb2239a247eb19
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-15 13:45:09 -0600

    2018 day 9 part 1 WIP

--- 2018/day9.py
+++ 2018/day9.py
@@ -1,4 +1,40 @@
+import re
+import numpy as np
+from itertools import cycle
+
+class Game:
+    REGEX = re.compile('(\d+) players; last marble is worth (\d+) points')
+
+    def __repr__(self):
+        return 'Circle: {}'.format(' '.join([str(m) for m in self.circle[:self.size]]))
+
+    def __init__(self, input):
+        match = self.REGEX.match(input)
+        self.player_count = int(match.group(1))
+        self.last_marble = int(match.group(2))
+        self.players = cycle(range(self.player_count))
+
+        self.scores = np.zeros(self.player_count, dtype=np.int32)
+        self.circle = np.zeros(self.last_marble, dtype=np.int32)
+        self.circle[0] = 0
+        self.marble = 1
+        self.size = 1
+        self.current_marble = 1
+
+    def take_turn(self):
+        self.size += 1
+        self.current_marble += 2
+        if self.current_marble > (self.size - 1):
+            self.current_marble -= self.size
+        self.circle[self.current_marble] = self.marble
+        self.marble += 1
+
+        next(self.players)
+
 def part1(input):
+    g = Game(input)
+    while g.marble < g.last_marble:
+        g.take_turn()
     return
 
 def part2(input):
