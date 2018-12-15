22cadb37b6a777bb388ea1da50b0da7cd9d359c7
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-15 14:49:49 -0600

    almost working, need to refactor a little

--- 2018/day9.py
+++ 2018/day9.py
@@ -6,7 +6,9 @@ class Game:
     REGEX = re.compile('(\d+) players; last marble is worth (\d+) points')
 
     def __repr__(self):
-        return 'Circle: {}'.format(' '.join([str(m) for m in self.circle]))
+        s = [str(m) for m in self.circle]
+        s[self.current_marble] = '({})'.format(s[self.current_marble])
+        return ' '.join(s)
 
     def __init__(self, input):
         match = self.REGEX.match(input)
@@ -25,18 +27,24 @@ class Game:
         return self._circle[:self.size]
 
     def insert(self, position, marble_val):
-        self.size += 1
-        if position >= self.size:
-            position = 1
-        self.circle[position+1:] = self.circle[position:-1]
-        self.circle[position] = marble_val
+        if marble_val % 23 == 0:
+            self.scores[self.player] += marble_val
+            position -= 7
+            self.scores[self.player] += self.circle[position]
+            self.circle[position:-1] = self.circle[position + 1:]
+            self.size -= 1
+        else:
+            self.size += 1
+            if position >= self.size:
+                position = 1
+            self.circle[position+1:] = self.circle[position:-1]
+            self.circle[position] = marble_val
         return position
 
     def take_turn(self):
         self.current_marble = self.insert(self.current_marble+2, self.marble)
         self.marble += 1
-
-        next(self.players)
+        self.player = next(self.players)
 
 def part1(input):
     g = Game(input)
@@ -46,7 +54,7 @@ def part1(input):
     # g.circle[:len(vals)] = vals
     # g.current_marble = 3
     # g.insert(5, 10)
-    while g.marble < g.last_marble:
+    while g.marble <= g.last_marble:
         g.take_turn()
     return
 
@@ -57,5 +65,7 @@ if __name__ == '__main__':
     import input as inp
     DAY = 9
     input = inp.read(DAY)
-    print(part1(input))
+    # print(part1(input))
     # print(part2(input))
+    # print(part1('10 players; last marble is worth 1618 points'))
+    print(part1('9 players; last marble is worth 7999 points'))
