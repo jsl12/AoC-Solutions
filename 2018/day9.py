2be3f53b81c5719d1a81299ccac3031d2736255a
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-15 12:50:32 -0600

    2018 day 9 stub

---
+++ 2018/day9.py
@@ -0,0 +1,12 @@
+def part1(input):
+    return
+
+def part2(input):
+    return
+
+if __name__ == '__main__':
+    import input as inp
+    DAY = 9
+    input = inp.read(DAY)
+    print(part1(input))
+    # print(part2(input))
