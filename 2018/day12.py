44e05a511fd9a548f8ddd20d1e74102a5745c951
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-16 12:49:48 -0600

    growing with regexes is working, but needs to wait until the end and do the growth in batches

--- 2018/day12.py
+++ 2018/day12.py
@@ -1,4 +1,30 @@
+import numpy as np
+import re
+
+class Garden:
+    INPUT_REGEX = re.compile('([\.#]{5}) => ([\.#])')
+    STATE_REGEX = re.compile('initial state: ([\.#]+)')
+
+    def __repr__(self):
+        return self.state
+
+    def __init__(self, input):
+        self.state = self.STATE_REGEX.search(input).group(1)
+        self.regex = {
+            re.compile('(?=({}))'.format(m[0].replace('.', '\.'))): m[1]
+            for m in self.INPUT_REGEX.findall(input)}
+
+    def grow(self, n):
+        for i in range(n):
+            for reg in self.regex:
+                for m in re.finditer(reg, self.state):
+                    start = m.span(1)[0]
+                    self.state =  self.state[:start+2] + self.regex[reg] + self.state[start+3:]
+                    continue
+
 def part1(input):
+    g = Garden(input)
+    g.grow(20)
     return
 
 def part2(input):
@@ -6,7 +32,24 @@ def part2(input):
 
 if __name__ == '__main__':
     import input as inp
-    DAY = 9
+    DAY = 12
     input = inp.read(DAY)
+#     input = '''initial state: #..#.#..##......###...###
+#
+# ...## => #
+# ..#.. => #
+# .#... => #
+# .#.#. => #
+# .#.## => #
+# .##.. => #
+# .#### => #
+# #.#.# => #
+# #.### => #
+# ##.#. => #
+# ##.## => #
+# ###.. => #
+# ###.# => #
+# ####. => #
+#     '''
     print(part1(input))
     # print(part2(input))
