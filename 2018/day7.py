0ea8da2afd6d9ec179ab34151c2f5b06b293f06a
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-18 14:04:28 -0600

    2018 day 7 part 1 solution

--- 2018/day7.py
+++ 2018/day7.py
@@ -6,24 +6,6 @@ def parse(line):
     match = REGEX.match(line)
     return match.group(1), match.group(2)
 
-class Step:
-    def __repr__(self):
-        if not hasattr(self, 'pre_steps'):
-            return '{} -> '.format(self.id)
-        else:
-            return '{} -> {}'.format(self.id, ', '.join(self.pre_steps))
-
-    def __init__(self, step, pre_steps=None):
-        self.id = step
-        if pre_steps is not None:
-            self.add_pre_step(pre_steps)
-
-    def add_pre_step(self, pre_step):
-        if not hasattr(self, 'pre_steps'):
-            self.pre_steps = set(pre_step)
-        else:
-            self.pre_steps.add(pre_step)
-
 def create_steps(input):
     steps = {}
     for i in [parse(line) for line in input.splitlines()]:
@@ -33,28 +15,23 @@ def create_steps(input):
             steps[i[1]] = [i[0]]
     return steps
 
-def start(steps):
+def find_start(steps):
     s = next(iter(steps.keys()))
     while s in steps:
         s = steps[s][0]
     return s
 
+def find_ready(steps, done):
+    res = sorted([s for s in steps if all([ps in done for ps in steps[s]])])
+    return res[0]
+
 def part1(input):
     steps = create_steps(input)
-    done = list(start(steps))
-
-    keys = iter(steps.keys())
-    s = next(keys)
-    remaining = steps.copy()
-    while len(done) < len(steps):
-        if all([i in done for i in steps[s]]):
-            done.append(s)
-            remaining.pop(s)
-        try:
-            s = next(keys)
-        except StopIteration as e:
-            keys = iter(list(remaining.keys()))
-            s = next(keys)
+    done = list(find_start(steps))
+    n = len(steps)
+    while len(done) <= n:
+        done.append(find_ready(steps, done))
+        steps.pop(done[-1])
     return ''.join(done)
 
 def part2(input):
