2e851e2a22fb3b26d3fec2c636ac0836d32dde8c
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-18 13:10:24 -0600

    generating Steps

--- 2018/day7.py
+++ 2018/day7.py
@@ -4,18 +4,37 @@ REGEX = re.compile('Step (\w) must be finished before step (\w) can begin.')
 
 def parse(line):
     match = REGEX.match(line)
-    return match.group(2), match.group(1)
+    return match.group(1), match.group(2)
 
 class Step:
     def __repr__(self):
-        return '{} -> {}'.format(self.pre_step, self.step)
+        if not hasattr(self, 'pre_steps'):
+            return '{} -> '.format(self.id)
+        else:
+            return '{} -> {}'.format(self.id, ', '.join(self.pre_steps))
 
-    def __init__(self, pre_step, step):
-        self.pre_step = pre_step
-        self.step = step
+    def __init__(self, step, pre_steps=None):
+        self.id = step
+        if pre_steps is not None:
+            self.add_pre_step(pre_steps)
+
+    def add_pre_step(self, pre_step):
+        if not hasattr(self, 'pre_steps'):
+            self.pre_steps = set(pre_step)
+        else:
+            self.pre_steps.add(pre_step)
+
+def create_steps(input):
+    steps = {}
+    for i in [parse(line) for line in input.splitlines()]:
+        if i[1] in steps:
+            steps[i[1]].append(i[0])
+        else:
+            steps[i[1]] = [i[0]]
+    return [Step(s, steps[s]) for s in steps]
 
 def part1(input):
-    input = [Step(*parse(line)) for line in input.splitlines()]
+    steps = create_steps(input)
     return
 
 def part2(input):
