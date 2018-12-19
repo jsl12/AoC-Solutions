30c99ff55b1d64f5a2a98b5c0921d5e632b6ef7c
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-19 15:45:20 -0600

    broke out functions

--- 2018/day8.py
+++ 2018/day8.py
@@ -1,31 +1,34 @@
 import numpy as np
 
+def parse(input):
+    return np.array([int(i) for i in input.split(' ')], dtype=np.uint8)
+
+def sum_node(input, iterator):
+    # assumes start of node
+    count_child = input[iterator.iterindex]
+    count_md = input[iterator.iterindex+1]
+    total = 0
+
+    iterator.iterindex += 2
+    if count_child > 0:
+        for i in range(count_child):
+            total += sum_node(input, iterator)
+    total += sum_metadata(input, iterator, count_md)
+    return total
+
+def sum_metadata(input, iterator, count):
+    # assumes start of metadata
+    start = iterator.iterindex
+    end = start + count
+    if iterator.itersize > end:
+        iterator.iterindex = end
+    else:
+        while not iterator.finished:
+            iterator.iternext()
+    return sum(input[start:end])
+
 def part1(input):
-    input = np.array([int(i) for i in input.split(' ')], dtype=np.uint8)
-
-    def sum_node(input, iterator):
-        # assumes start of node
-        count_child = input[iterator.iterindex]
-        count_md = input[iterator.iterindex+1]
-        total = 0
-
-        iterator.iterindex += 2
-        if count_child > 0:
-            for i in range(count_child):
-                total += sum_node(input, iterator)
-        total += sum_metadata(input, iterator, count_md)
-        return total
-
-    def sum_metadata(input, iterator, count):
-        # assumes start of metadata
-        start = iterator.iterindex
-        end = start + count
-        if iterator.itersize > end:
-            iterator.iterindex = end
-        else:
-            while not iterator.finished:
-                iterator.iternext()
-        return sum(input[start:end])
+    input = parse(input)
 
     with np.nditer(input, op_flags=['readonly'], flags=['f_index']) as it:
         while not it.finished:
