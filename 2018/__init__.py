cd5784d959a708fbf9dd86481e74127e6bace2a9
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-19 15:40:47 -0600

    fleshed out register and init

--- 2018/__init__.py
+++ 2018/__init__.py
@@ -4,9 +4,22 @@ from . import day3
 from . import day4
 from . import day5
 from . import day6
-# from . import day7
+from . import day7
 from . import day8
 from . import day9
 from . import day10
 from . import day11
 from . import day12
+# from . import day13
+# from . import day14
+# from . import day15
+# from . import day16
+# from . import day17
+# from . import day18
+# from . import day19
+# from . import day20
+# from . import day21
+# from . import day22
+# from . import day23
+# from . import day24
+# from . import day25
