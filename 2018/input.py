71b44fc3f518ed7d433e242ea046754ca09c3254
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-20 01:25:55 -0600

    fixed the input glob

--- 2018/input.py
+++ 2018/input.py
@@ -5,7 +5,7 @@ p = Path(r'C:\Users\lanca_000\Documents\Software\Python\AoC Benchmark')
 cfg = config.Config(p / 'users.yaml')
 
 def read(i):
-    file = [f for f in cfg.inputs_dir.glob('2018/*day*{}.txt'.format(i))][0]
+    file = [f for f in cfg.inputs_dir.glob('2018/*day{}.txt'.format(i))][0]
     print('Getting input for 2018 Day {} from:\n{}'.format(i, file))
     with open(file, 'r') as f:
         return f.read()
