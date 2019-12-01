805fe041a84306b64dff6c4fdf827f53d10b7434
jsl12 <32917998+jsl12@users.noreply.github.com>
2019-12-01 16:46:05 -0600

    turned 2019 day 1 into generators

--- 2019/day1.py
+++ 2019/day1.py
@@ -11,18 +11,18 @@ def fuel_needed(mass, fuel_total:int = 0):
 
 
 def part1(input):
-    res = sum([floor(int(line) / 3) - 2 for line in input.splitlines()])
+    res = sum([floor(int(line) / 3) - 2 for line in input])
     return res
 
 
 def part2(input):
-    res = sum([fuel_needed(line) for line in input.splitlines()])
+    res = sum([fuel_needed(line) for line in input])
     return res
 
 
 if __name__ == '__main__':
     import input as inp
+
     DAY = 1
-    input = inp.read(DAY)
-    print(part1(input))
-    print(part2(input))
+    print(part1(inp.day_gen(DAY)))
+    print(part2(inp.day_gen(DAY)))
--- 2019/input.py
+++ 2019/input.py
@@ -4,8 +4,8 @@ from pathlib import Path
 p = Path(r'C:\Users\lanca_000\Documents\Software\Python\AoC Benchmark')
 cfg = config.Config(p / 'users.yaml')
 
-def read(i):
-    file = [f for f in cfg.inputs_dir.glob('2019/*day{}.txt'.format(i))][0]
-    print('Getting input for 2018 Day {} from:\n{}'.format(i, file))
-    with open(file, 'r') as f:
-        return f.read()
+def day_gen(day_num:int):
+    file = [f for f in cfg.inputs_dir.glob(f'2019/*day{day_num}.txt')][0]
+    print(f'Getting input for 2019 Day {day_num} from:\n{file}')
+    with file.open('r') as f:
+        return (line for line in f.readlines())
