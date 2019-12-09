d1279e310ff709b1ab7df5d6a390fca5b2c3c0bb
jsl12 <32917998+jsl12@users.noreply.github.com>
2019-12-09 13:22:48 -0600

    2019 day 4 part 1 solution

--- 2019/day4.py
+++ 2019/day4.py
@@ -7,22 +7,26 @@ def rule1_check(input):
                 break
     return res
 
-def rule2_check(input):
-    for i in range(9):
-        if input.count(str(i)) >= 2:
-            return True
-    return False
+
+def rule2_check(input, count_threshold = 2):
+    count = 1
+    max_count = 0
+    for i, c in enumerate(input):
+        try:
+            if input[i+1] == c:
+                count += 1
+                if count > max_count:
+                    max_count = count
+            else:
+                count = 1
+        except IndexError:
+            pass
+    return max_count >= count_threshold
 
 def part1(input):
     limits = [line for line in input.split('-')]
-    count = 0
-    with open('day4_sols.txt', 'w') as file:
-        for i in range(int(limits[0]), int(limits[1])):
-            i = str(i)
-            if rule1_check(i) and rule2_check(i):
-                count += 1
-                file.write(f'{i}\n')
-    return count
+    res = [i for i in range(int(limits[0]), int(limits[1])) if rule1_check(str(i)) and rule2_check(str(i), 2)]
+    return len(res)
 
 def part2(input):
     return
