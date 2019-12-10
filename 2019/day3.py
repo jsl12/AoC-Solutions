0ddaf4bb25f2207d2775f477070200be218f80a0
jsl12 <32917998+jsl12@users.noreply.github.com>
2019-12-09 23:54:24 -0600

    2019 day 3 part 2 progress - sample 1 working

--- 2019/day3.py
+++ 2019/day3.py
@@ -33,6 +33,20 @@ class Wire:
         res = [min_dist(seg, other_wire) for seg in self.segments]
         return min([r for r in res if r is not None])
 
+    def intersections(self, other_wire):
+        res = []
+        [res.extend(seg.wire_intersection(other_wire)) for seg in self.segments]
+        return res
+
+    def distance_to_intersection(self, x, y):
+        res = 0
+        for seg in self.segments:
+            if seg.check_point(x, y):
+                res += abs(x - seg.start[0]) + abs(y - seg.start[1])
+                break
+            else:
+                res += seg.len
+        return res
 
 def min_dist(seg, wire):
     intersections = seg.wire_intersection(wire)
@@ -88,6 +102,9 @@ class Segment:
             y4 = seg.end[1],
         )
 
+    def check_point(self, x, y):
+        return (self.start[0] <= x <= self.end[0]) and (self.start[1] <= y <= self.end[1])
+
 
 def part1(input):
     wires = [Wire(line).draw_all() for line in input.splitlines()]
@@ -95,12 +112,18 @@ def part1(input):
 
 
 def part2(input):
-    return
+    wires = [Wire(line).draw_all() for line in input.splitlines()]
+    crosses = wires[0].intersections(wires[1])
+    res = [wires[0].distance_to_intersection(*c) + wires[1].distance_to_intersection(*c) for c in crosses]
+    return min(res)
 
 
 if __name__ == '__main__':
     import aoc_input as inp
     DAY = 3
-    print(part1('R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83'))
+    sample1 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83'
+    sample2 = 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
+    print(part2(sample1))
+    print(part2(sample2))
     print(part1(inp.read(DAY)))
     # print(part2(inp.read(DAY)))
