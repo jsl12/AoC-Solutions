aeb6804ca53bde2db3e3ff85e9b869ae502de35a
jsl12 <32917998+jsl12@users.noreply.github.com>
2019-12-10 00:24:07 -0600

    2019 day 3 part 2 solution

--- 2019/day3.py
+++ 2019/day3.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from utils import segment_intersection
 
 
@@ -8,6 +9,7 @@ class Wire:
         self.segments = [Segment(i) for i in input.split(',')]
         self.vertical_segments = [seg for seg in self.segments if seg.vertical]
         self.horizontal_segments = [seg for seg in self.segments if not seg.vertical]
+        self.draw_all()
 
     def __repr__(self):
         return self.input
@@ -103,19 +105,32 @@ class Segment:
         )
 
     def check_point(self, x, y):
-        return (self.start[0] <= x <= self.end[0]) and (self.start[1] <= y <= self.end[1])
+        min_x = min([self.start[0], self.end[0]])
+        max_x = max([self.start[0], self.end[0]])
+        min_y = min([self.start[1], self.end[1]])
+        max_y = max([self.start[1], self.end[1]])
+        return (min_x <= x <= max_x) and (min_y <= y <= max_y)
 
 
 def part1(input):
-    wires = [Wire(line).draw_all() for line in input.splitlines()]
+    wires = [Wire(line) for line in input.splitlines()]
     return wires[0].closest_intersection(wires[1])
 
 
 def part2(input):
-    wires = [Wire(line).draw_all() for line in input.splitlines()]
+    wires = [Wire(line) for line in input.splitlines()]
     crosses = wires[0].intersections(wires[1])
     res = [wires[0].distance_to_intersection(*c) + wires[1].distance_to_intersection(*c) for c in crosses]
     return min(res)
+
+
+def visualize(input):
+    wires = [Wire(line) for line in input.splitlines()]
+    fig, ax = plt.subplots()
+    for w in wires:
+        ax.plot(w.series)
+    ax.grid(True)
+    fig.savefig('day3.png')
 
 
 if __name__ == '__main__':
@@ -125,5 +140,6 @@ if __name__ == '__main__':
     sample2 = 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
     print(part2(sample1))
     print(part2(sample2))
+    # visualize(sample2)
     print(part1(inp.read(DAY)))
-    # print(part2(inp.read(DAY)))
+    print(part2(inp.read(DAY)))
