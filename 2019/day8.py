6a4e213c13881177c1f461f2e3b9f2a284e7bcf5
jsl12 <32917998+jsl12@users.noreply.github.com>
2019-12-14 14:58:00 -0600

    2019 day 8 part 1 solution

---
+++ 2019/day8.py
@@ -0,0 +1,56 @@
+import textwrap
+from dataclasses import dataclass
+from typing import Tuple
+
+
+@dataclass
+class Image:
+    input: str
+    dims: Tuple[int, int]
+
+    @property
+    def size(self):
+        return self.dims[0] * self.dims[1]
+
+    def __post_init__(self):
+        self.layers = [Layer(input, self.dims) for input in textwrap.wrap(self.input, self.size)]
+
+    def counts(self, char):
+        return [layer.count(char) for layer in self.layers]
+
+    def fewest_layer(self, char):
+        counts = self.counts(char)
+        return self.layers[counts.index(min(counts))]
+
+
+@dataclass
+class Layer:
+    input: str
+    dims: Tuple[int, int]
+
+    def count(self, char):
+        return self.input.count(char)
+
+
+def part1(input):
+    """
+    >>> import aoc_input as inp; part1(inp.read(8))
+    1215
+    """
+    img = Image(input, (25, 6))
+    layer = img.fewest_layer('0')
+    return layer.count('1') * layer.count('2')
+
+
+def part2(input):
+    return
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    import aoc_input as inp
+
+    DAY = 8
+    print(part1(inp.read(DAY)))
+    print(part2(inp.read(DAY)))
