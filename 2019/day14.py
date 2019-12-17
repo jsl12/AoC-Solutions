fdfe7807dc74c91c488254cb9ce914d26edfe0d6
jsl12 <32917998+jsl12@users.noreply.github.com>
2019-12-17 01:32:18 -0600

    2019 day 14 part 2 solution

--- 2019/day14.py
+++ 2019/day14.py
@@ -52,6 +52,19 @@ class ReactionSystem:
 
         return ore_total
 
+    def max_fuel(self, ore=1000000000000):
+        per_fuel = self.ore_needed('FUEL')
+        num_reactions = 1
+        ore_consumed = self.ore_needed('FUEL', num_reactions)
+        while ore > ore_consumed:
+            new_reactions = math.floor((ore - ore_consumed) / per_fuel)
+            if new_reactions > 0:
+                num_reactions += new_reactions
+            else:
+                num_reactions += 1
+            ore_consumed = self.ore_needed('FUEL', num_reactions)
+        return num_reactions - 1
+
 
 class Reaction:
     regex = re.compile('([\d]+) ([\w]+)')
@@ -62,15 +75,13 @@ class Reaction:
         m = self.regex.search(output)
         self.output = (int(m.group(1)), m.group(2))
 
-        # g = reduce(math.gcd, self.amounts)
-        return
-
     def __repr__(self):
         return self.str
 
     @property
     def amounts(self):
         return [r[0] for r in self.input] + [self.output[0]]
+
 
 def part1(input):
     """
@@ -82,14 +93,15 @@ def part1(input):
 
 
 def part2(input):
-    return
+    """
+    >>> import aoc_input as inp; part2(inp.read(14))
+    5650230
+    """
+    rs = ReactionSystem(input)
+    ratio = rs.ore_needed('FUEL')
+    return rs.max_fuel()
 
 
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
-    import aoc_input as inp
-
-    DAY = 14
-    # print(part1(inp.read(DAY)))
-    print(part2(inp.read(DAY)))
