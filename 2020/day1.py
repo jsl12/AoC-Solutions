059363e880c112fceb762459d9d6d2f120db53d9
jsl12 <lancaster.js@gmail.com>
2020-12-16 17:07:56 -0600

    2020 day 1 part 2 solution

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,25 +1,22 @@
 import numpy as np
 from typing import Iterable
+from itertools import combinations
+from functools import reduce
 
-def part1(nums: Iterable):
-    for n in nums:
-        added = n + nums
-        try:
-            idx = np.where(added==2020)[0][0]
-        except IndexError:
-            continue
-        else:
-            return n * nums[idx]
+def part1(nums, num_items=2, target=2020):
+    for combo in combinations(nums, num_items):
+        if sum(combo) == target:
+            break
+    return reduce(lambda x,y: x*y, combo)
 
 
-def part2(input):
-    return
+def part2(nums):
+    return part1(nums, num_items=3)
 
 
 if __name__ == '__main__':
     import aoc_input as inp
-
     DAY = 1
-    nums = np.array([n for n in inp.read_nums(DAY)])
-    print(part1(nums))
-    print(part2(inp.read(DAY)))
+    input_nums = np.array([n for n in inp.read_nums(DAY)])
+    print(part1(input_nums))
+    print(part2(input_nums))
