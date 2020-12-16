7de154efb8f1b957081dfc96a59fb0e1ad741414
jsl12 <lancaster.js@gmail.com>
2020-12-16 17:53:45 -0600

    2020 day 2 part 2 solution

--- 2020/day2.py
+++ 2020/day2.py
@@ -1,19 +1,36 @@
+def parse_input_line(line):
+    params, password = line.split(': ')
+    params, char = params.split(' ')
+    param1, param2 = params.split('-')
+    return int(param1), int(param2), char, password
+
+
 def part1(lines):
     res = 0
     for line in lines:
-        params, password = line.split(': ')
-        params, char = params.split(' ')
-        min, max = params.split('-')
-        valid = int(min) <= password.count(char) <= int(max)
+        min, max, char, password = parse_input_line(line)
+        valid = min <= password.count(char) <= max
         if valid:
             res += 1
     return res
 
+
+def part2_check(line):
+    pos1, pos2, char, password = parse_input_line(line)
+    return bool((password[pos1 - 1] == char)  != (password[pos2 - 1] == char))
+
+
 def part2(lines):
-    return
+    res = 0
+    for line in lines:
+        if part2_check(line):
+            res += 1
+    return res
+
 
 if __name__ == '__main__':
     import aoc_input as inp
     DAY = 2
+    part2_check('2-9 c: ccccccccc')
     print(part1(inp.read(DAY)))
-    # print(part2(input_nums))
+    print(part2(inp.read(DAY)))
