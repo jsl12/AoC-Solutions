9bfec8dbec63a22c29c43e81c02c61e9d73aa6ff
jsl12 <32917998+jsl12@users.noreply.github.com>
2021-12-27 08:09:35 +0100

    2021 day 10 part 2 solution

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from aoc_input import read
+
 
 tokens = {
     '(': ')',
@@ -7,26 +10,77 @@ tokens = {
     '[': ']'
 }
 
-scores = {
-    ')': 3,
-    ']': 57,
-    '}': 1197,
-    '>': 25137
-}
 
-def part1(inp):
-    line_length = max(map(len, inp))
+class Line:
+    def __init__(self, line:str):
+        self.chars = list(line)
+        self.open = []
+    
+    def __repr__(self):
+        return ''.join(self.chars)
+    
+    def validate(self):
+        for char in self.chars:
+            if char in tokens:
+                self.open.append(char)
+            elif char == tokens[self.open[-1]]:
+                self.open.pop(-1)
+            else:
+                return False
+        else:
+            return True
+
+    def closers(self):
+        return [tokens[char] for char in self.open][::-1]
+    
+    def score(self):
+        scores = {
+            ')': 1,
+            ']': 2,
+            '}': 3,
+            '>': 4
+        }
+        total = 0
+        for char in self.closers():
+            total *= 5
+            total += scores[char]
+        return total
+
+
+def check_line(line: str):
+    scores = {
+        ')': 3,
+        ']': 57,
+        '}': 1197,
+        '>': 25137
+    }
+    open_chars = []
+    for char in line:
+        if char in tokens:
+            open_chars.append(char)
+        elif char == tokens[open_chars[-1]]:
+            open_chars.pop(-1)
+        else:
+            points = scores[char]
+            return char, points
+    else:
+        return open_chars
+
+    
+def part1(inp: List[str]):
     score = 0
     for line in inp:
-        open_chars = []
-        for char in line:
-            if char in tokens:
-                open_chars.append(char)
-            elif char == tokens[open_chars[-1]]:
-                open_chars.pop(-1)
-            else:
-                points = scores[char]
-                # print(f'{line.ljust(line_length)} - Expected {tokens[open_chars[-1]]}, but found {char} instead, {points}')
-                score += points
-                break
+        try:
+            char, points = check_line(line)
+        except:
+            continue
+        else:
+            score += points
     return score
+
+
+def part2(inp: List[str]):
+    lines = [Line(line) for line in inp]
+    lines = [line for line in lines if line.validate()]
+    totals = sorted([line.score() for line in lines])
+    return totals[int(len(totals) / 2)]
