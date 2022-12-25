952a0a4ac110d28f2b0e733c808f3c152fee0a0b
John Lancaster <32917998+jsl12@users.noreply.github.com>
2022-12-24 18:30:59 -0600

    2022 day1 part1

---
+++ 2022/day1.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from aoc_input import find_day
+
+def elf_gen(lines):
+    elf_total = 0
+    for line in lines:
+        try:
+            elf_total += int(line)
+        except:
+            yield elf_total
+            elf_total = 0
+
+if __name__ == '__main__':
+    this_file = Path(__file__)
+    path = this_file.parents[2] / f'inputs/2022/{this_file.stem}.txt'
+    lines = path.read_text().splitlines()
+
+    print(max(elf_gen(lines)))
